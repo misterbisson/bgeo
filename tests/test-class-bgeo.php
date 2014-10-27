48b207b917816a57800d516cf4d274adca4c2f07
Casey Bisson <casey.bisson@gmail.com>
2014-10-27 14:37:19 -0700

    initial unit test scaffold
    See http://maisonbisson.com/post/17645/unit-testing-wordpress-plugins/

---
+++ tests/test-class-bgeo.php
@@ -0,0 +1,12 @@
+<?php
+
+class bGeo_Test extends WP_UnitTestCase
+{
+	/**
+	 * which tests the constructor, the init action, etc...
+	 */
+	public function test_singleton()
+	{
+		$this->assertTrue( is_object( bgeo() ) );
+	}//end test_singleton
+}
